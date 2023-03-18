129ff653c7463ced41db34c55dc0d45f52b704e9
ZakDH <60523522+ZakDH@users.noreply.github.com>
2023-03-18 16:08:51 +0000

    added withdrawal function

---
+++ main.py
@@ -0,0 +1,54 @@
+import random
+
+class Account:
+    def __init__(self, account_number, balance=0):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def checkBalance(self):
+        return self.__balance
+    
+    def withdraw(self, amount):
+        if self.__balance < amount:
+            raise ValueError("Insufficient balance, please re-enter a withdrawal amount...")
+        self.__balance -= amount
+
+def createAccount(name):
+    account_number = str(random.randint(100000, 999999))
+    account = Account(account_number)
+    print(f"Account created for {name} with the account number {account_number}")
+    return account
+
+
+# Example usage
+name = input("What is your first name?: ")
+account = createAccount(name)
+while True:
+    deposit = input("How much do you want to deposit into your new account?: ")
+    try:
+        intDeposit = int(deposit)
+        break;
+    except ValueError:
+        try:
+            float(deposit)
+            break;
+        except ValueError:
+            print("Please enter a valid amount to be deposited...")
+account.deposit(intDeposit)
+print(account.checkBalance())
+
+while True:
+    withdraw = input("How much do you want to withdraw from your new account?: ")
+    try:
+        intWithdraw = int(withdraw)
+        break;
+    except ValueError:
+        try:
+            float(withdraw)
+            break;
+        except ValueError:
+            print("Please enter a valid amount to be deposited...")
+account.withdraw(intWithdraw)
+print(account.checkBalance())
